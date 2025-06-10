6399cf460d9432233a9dbc12bc576ef9409a9807
christi903 <christiansahili01@gmail.com>
2025-06-10 22:03:02 +0300

    new auth

--- server/config/supabaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-// Use environment variables
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // Service role is required for admin tasks
-);
-
-module.exports = supabase;
